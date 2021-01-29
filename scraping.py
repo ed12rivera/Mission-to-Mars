055c787669e1f4c485db6fe05a17b5cd91863d94
unknown <ed12rivera@gmail.com>
2021-01-29 01:56:36 -0800

    Update scraping code.

--- scraping.py
+++ scraping.py
@@ -1,116 +1,87 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
+def scrape_all():
+    # Initiate the headless driver for deployment
+    # Set the executable path and initialize the chrome browser in splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
-# In[2]:
+    news_title, news_paragraph = mars_news(browser)
 
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
 
-# Set the executable path and initialize the chrome browser in splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path)
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+    # Stop webdriver and return datae
+    browser.quit()
+    return data
 
-
-# In[3]:
-
-
-# Visit the mars nasa news site
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
-# What does is_element_not_present_by_css mean
-browser.is_element_not_present_by_css("ul.item_list li.slide", wait_time=1)
-
-
-# In[4]:
-
-
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('ul.item_list li.slide')
-
-
-# In[5]:
-
-
-slide_elem.find("div", class_='content_title')
-
-
-# In[6]:
-
-
-# Use the parent element to find the first 'a' tag and save it as 'news_title'
-news_title = slide_elem.find("div", class_='content_title').get_text()
-news_title
-
-
-# In[7]:
-
-
-# Use parent element to find summary of news article
-summary = slide_elem.find("div", class_='article_teaser_body').get_text()
-summary
-
-
-# ### Featured Images
-
-# In[8]:
-
-
-# Visit Archived JPL URL
-PREFIX = "https://web.archive.org/web/20181114023740"
-url = f'{PREFIX}/https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(url)
-
-
-# In[9]:
-
-
-try:
-    PREFIX = "https://web.archive.org/web/20181114023740"
-    url = f'{PREFIX}/https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+def mars_news(browser):
+    # Visit the mars nasa news site
+    url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
-    article = browser.find_by_tag('article').first['style']
-    article_background = article.split("_/")[1].replace('");',"")
-    print (f'{PREFIX}_if/{article_background}')
-except:
-    print('https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia22486-main.jpg')
+    # Wait one second to load the page
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('ul.item_list li.slide')
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find("div", class_='content_title').get_text()
+
+        # Use parent element to find summary of news article
+        summary = slide_elem.find("div", class_='article_teaser_body').get_text()
+
+    except AttributeError:
+        return None, None
+
+    return news_title, summary
 
 
-# ## Mars Info Table
+### Featured Images
+def featured_image(browser):
+    # Visit Archived JPL URL
+    try:
+        PREFIX = "https://web.archive.org/web/20181114023740"
+        url = f'{PREFIX}/https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+        browser.visit(url)
+        article = browser.find_by_tag('article').first['style']
+        article_background = article.split("_/")[1].replace('");',"")
+        return (f'{PREFIX}_if/{article_background}')
+    except:
+        return('https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia22486-main.jpg')
+    
+## Mars Info Table
+def mars_facts():
+    try:
+        # use 'read_html' to scrape the facts table into a dataframe
+        df = pd.read_html('https://space-facts.com/mars/')[0]
+        
+    except BaseException:
+        return None
+    
+    # Assign columns and set index of dataframe
+    df.columns=['description', 'value']
+    df.set_index('description', inplace=True)
 
-# In[10]:
-
-
-df = pd.read_html('https://space-facts.com/mars/')[0]
-df.columns=['description', 'value']
-df.set_index('description', inplace=True)
-df
-
-
-# In[11]:
-
-
-df.to_html()
-
-
-# In[12]:
-
-
-browser.quit()
-
-
-# In[ ]:
-
-
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html
+    
+ if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
 
 
